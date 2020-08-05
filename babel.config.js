b7d38375af1d99cea32ddf208cb40418f6355c22
Darío Javier Cravero <dario@uxtemple.com>
2020-08-05 11:13:30 +0200

    feat: add views on top of it

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
-module.exports = function(api) {
-  api.cache(true);
+let fs = require('fs')
+
+module.exports = function (api) {
+  api.cache(true)
+
+  // Support absolute imports from src like
+  // https://create-react-app.dev/docs/importing-a-component/#absolute-imports
+  let alias = {}
+  fs.readdirSync('./src').forEach(item => {
+    alias[item] = `./src/${item}`
+  })
+
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [['module-resolver', { alias }]],
+  }
+}
---
+++ src/logic.js
@@ -0,0 +1,29 @@
+import { useFonts } from 'expo-font';
+import { ViewsFlow } from 'Logic/ViewsFlow.js';
+import React from 'react';
+import View from './view.js';
+
+export default function Logic(props) {
+  let [loaded] = useFonts({
+    // At some point, Views will do this automatically. For now, you
+    // need to write your fonts by hand.
+    //
+    // 1) Download the font into src/DesignSystem/Fonts. The name of the file is FontFamily-Weight.ttf
+    // 2) Use it in a view:
+    //    Text
+    //    fontFamily Robot Mono
+    //    fontWeight 300
+    //    text hey I'm using Roboto Mono!
+    // 3) Add it to this object like:
+    //
+    // 'Montserrat-300': require('../DesignSystem/Fonts/Montserrat-300.ttf'),
+  });
+
+  if (!loaded) return null
+
+  return (
+    <ViewsFlow>
+      <View {...props} />
+    </ViewsFlow>
+  );
+}
